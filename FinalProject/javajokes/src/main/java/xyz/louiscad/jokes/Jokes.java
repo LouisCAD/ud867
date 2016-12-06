cb4505747ad1b4d5d2093c72c57652dd3c9539bc
Louis Cognault <louis.cognault@gmail.com>
2016-12-06 09:21:03 +0100

    Replace derp with the joke from the java library

---
+++ FinalProject/javajokes/src/main/java/xyz/louiscad/jokes/Jokes.java
@@ -0,0 +1,5 @@
+package xyz.louiscad.jokes;
+
+public class Jokes {
+    public static final String KANGAROO_JUMP = "Can a kangaroo jump higher than a house? Of course, a house doesn’t jump at all.";
+}
