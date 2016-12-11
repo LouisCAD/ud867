2d666954503ed1c1f37ecfa838fbb629e29c19b3
Louis Cognault <louis.cognault@gmail.com>
2016-12-11 23:21:30 +0100

    Add free (with ads) & paid (no ads) product flavors

--- FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -8,8 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
@@ -35,15 +33,7 @@ public class MainActivityFragment extends Fragment implements View.OnClickListen
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
-
-        AdView mAdView = (AdView) root.findViewById(R.id.adView);
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
-        mAdView.loadAd(adRequest);
+        AutoLoadingAdView.loadAdIfNotPaid(root);
         root.findViewById(R.id.tellJokeButton).setOnClickListener(this);
         return root;
     }
@@ -92,6 +82,7 @@ public class MainActivityFragment extends Fragment implements View.OnClickListen
         private static MyApi sApiService = null;
         private static final String ROOT_URL_USB_HOTSPOT = "http://192.168.42.220:8080/_ah/api/";
         private static final String ROOT_URL_HOME = "http://192.168.86.35:8080/_ah/api/";
+        private static final String ROOT_URL_EMULATOR = "http://10.0.2.2:8080/_ah/api/";
 
         @Override
         protected String doInBackground(Void... params) {
---
+++ FinalProject/app/src/paid/java/com/udacity/gradle/builditbigger/AutoLoadingAdView.java
@@ -0,0 +1,10 @@
+package com.udacity.gradle.builditbigger;
+
+import android.view.View;
+
+public enum  AutoLoadingAdView {
+    ;
+    public static void loadAdIfNotPaid(View root) {
+        //We are in the paid productFlavor, so no ad to show here.
+    }
+}
